a41658243d4de2d0c57c6f06c23177b94d43a984
Momtchil Momtchev <momtchil@momtchev.com>
2022-03-12 10:35:35 +0100

    add copyright mentions

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,22 @@ import AirHumidity from './comp/AirHumidity';
 
 function App() {
   return (
-    <div className="velitherm-visu">
-      <header className="header">
-        Air Humidity
-      </header>
-      <AirHumidity />
-    </div>
+    <React.Fragment>
+      <div className="velitherm-visu">
+        <header className="header">
+          Air Humidity
+        </header>
+        <AirHumidity />
+      </div>
+      <footer className='m-4'>
+        <small>
+          <p>All values computed by <a href="https://github.com/mmomtchev/velitherm">velitherm</a>, Momtchil Momtchev, <a href="https://www.meteo.guru">meteo.guru</a>/<a href="https://www.velivole.fr">velivole.fr</a>, 2022</p>
+          <p><a href="https://github.com/mmomtchev/velitherm-visu">Source code</a> under GPL license</p>
+          <p>buttons by KP Arts from NounProject.com</p>
+          <p>Build from {process.env.REACT_APP_BUILD}</p>
+        </small>
+      </footer>
+    </React.Fragment>
   );
 }
 
