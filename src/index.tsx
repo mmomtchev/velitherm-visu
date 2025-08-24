c63ff15e5c681fcfc7e7b670a7932fe742192266
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-24 13:04:59 +0200

    restore the automatic language

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import fr from './compiled-lang/fr.json';
 import en from './compiled-lang/en.json';
 
 const userLang = (navigator.language || 'en-US').split('-')[0];
-//const siteLang = (['fr', 'en'].includes(userLang) ? userLang : 'en') as 'fr' | 'en';
+const siteLang = (['fr', 'en'].includes(userLang) ? userLang : 'en') as 'fr' | 'en';
 const messages = { fr, en };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <IntlProvider
       locale={userLang}
       defaultLocale="en"
-      messages={messages['fr']}
+      messages={messages[siteLang]}
     >
       <App />
     </IntlProvider>
