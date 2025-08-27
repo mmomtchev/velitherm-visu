f59aa9ea1ba354a60be6ee59c0c389d8bbc4f561
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-27 18:26:11 +0200

    allow to change the language via ?lang=

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, useSearchParams } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 import './index.css';
 import App from './App';
-
-import { IntlProvider } from 'react-intl';
 
 import fr from './compiled-lang/fr.json';
 import en from './compiled-lang/en.json';
@@ -12,14 +12,26 @@ const userLang = (navigator.language || 'en-US').split('-')[0];
 const siteLang = (['fr', 'en'].includes(userLang) ? userLang : 'en') as 'fr' | 'en';
 const messages = { fr, en };
 
+// eslint-disable-next-line react-refresh/only-export-components
+const IntlApp = () => {
+  const [searchParams] = useSearchParams();
+  const queryLang = searchParams.get('lang');
+  const userLang = (navigator.language || 'en-US').split('-')[0];
+  const siteLang = (queryLang ?? (['fr', 'en'].includes(userLang) ? userLang : 'en')) as 'fr' | 'en';
+
+  return <IntlProvider
+    locale={userLang}
+    defaultLocale="en"
+    messages={messages[siteLang]}
+  >
+    <App />
+  </IntlProvider>;
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <IntlProvider
-      locale={userLang}
-      defaultLocale="en"
-      messages={messages[siteLang]}
-    >
-      <App />
-    </IntlProvider>
+    <BrowserRouter>
+      <IntlApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
